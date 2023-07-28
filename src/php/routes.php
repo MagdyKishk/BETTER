968d1f21fe1791f217782fe639ffc529ad870a9c
Magdy Tantawy Kishk <112126952+MagdyKishk@users.noreply.github.com>
2023-07-28 17:02:23 +0300

    Added Routing File In ./src/php/routes.php also made changes on index.php to manage routing using routes.php

---
+++ src/php/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+	class page_route
+	{
+		/* Routing list */
+		protected $routes = [
+			'GET' => [
+				'/'       => 'home.php',
+				'/home'   => 'home.php',
+				'/login'  => 'login.php',
+				'/signup' => 'signup.php',
+			],
+			'POST' => [
+				'/login'  => 'login.php',
+				'/signup' => 'signup.php',
+			],
+		];
+
+		/**
+		 * route_to_page - redirect the user to the corresponding page to
+		 * the URL path and method
+		 * Return: void;
+		 */
+		public function route_to_page() : void {
+			/* Gets the path from the current url */
+			$current_url = parse_url($_SERVER["REQUEST_URI"])['path'];
+			/* Gets the request method */
+			$method = $_SERVER["REQUEST_METHOD"];
+
+			if (isset($this->routes[$method][$current_url])) {
+				require_once 'pages/' . $this->routes[$method][$current_url];
+			} else {
+				require_once 'pages/error_pages/404.php';
+				http_response_code(404);
+			}
+		}
+	};
