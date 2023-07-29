2aa92ca6699322d90ea3a4b720f40cd55ea1bcc4
Magdy Tantawy Kishk <112126952+MagdyKishk@users.noreply.github.com>
2023-07-29 10:54:25 +0300

    Added Navbar ./pages/components/navbar.php Added Font Awesome ./src/lib/fontawesome-free-6.4.0-web Added Css Main Files at ./src/css/* Added headers php template files ./pages/headers

---
+++ pages/components/navbar.php
@@ -0,0 +1,57 @@
+<header class="navbar">
+	<div class="container">
+		<a href="/home" class="logo">
+		<span class="logo_circle">B</span>ETTER
+		</a>
+		<nav>
+			<ul>
+				<li>
+					<a href="/home">
+						<div class="icon">
+							<i class="fa-solid fa-house"></i>
+						</div>
+						Home
+					</a>
+				</li>
+				<li>
+					<a href="/trends">
+						<div class="icon">
+							<i class="fa-solid fa-arrow-trend-up"></i>
+						</div>
+						Trends
+					</a>
+				</li>
+				<li>
+					<a href="/user">
+						<div class="icon">
+							<i class="fa-solid fa-user"></i>
+						</div>
+						Profile
+					</a>
+				</li>
+				<li>
+					<a href="/settings">
+						<div class="icon">
+							<i class="fa-solid fa-gear"></i>
+						</div>
+						Settings
+					</a>
+				</li>
+				<li>
+					<a href="/logout">
+						<div class="icon">
+							<i class="fa-solid fa-arrow-right-from-bracket"></i>
+						</div>
+						Log out
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<form action="">
+			<input type="text" name="username-search" placeholder="Search for People">
+			<button type="submit" class="icon">
+				<i class="fa-solid fa-magnifying-glass"></i>
+			</button>
+		</form>
+	</div>
+</header>
---
+++ pages/headers/font-awesome-header.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="./src/lib/fontawesome-free-6.4.0-web/css/all.min.css">
+<script defer src="./src/lib/fontawesome-free-6.4.0-web/js/all.min.js"></script>
