47bcd95f683bd15a1d41b83ee560a22e570a19c3
Eyad Shanshan <eyadshanshan@gmail.com>
2023-08-01 16:23:03 +0300

    Reconstructed nav-bar [Font-End]

--- pages/components/navbar.php
+++ pages/components/navbar.php
@@ -1,57 +1,40 @@
-<header class="navbar">
-	<div class="container">
+<header class="nav-bar">
+
+	<div class="menu_brand">
+		<button class="burger">
+			<i class="fa-solid fa-bars"></i>
+		</button>
+
 		<a href="/home" class="logo">
-		<span class="logo_circle">B</span>ETTER
+			<span class="logo_circle">B</span>ETTER
 		</a>
-		<nav>
-			<ul>
-				<li>
-					<a href="/home">
-						<div class="icon">
-							<i class="fa-solid fa-house"></i>
-						</div>
-						Home
-					</a>
-				</li>
-				<li>
-					<a href="/trends">
-						<div class="icon">
-							<i class="fa-solid fa-arrow-trend-up"></i>
-						</div>
-						Trends
-					</a>
-				</li>
-				<li>
-					<a href="/user">
-						<div class="icon">
-							<i class="fa-solid fa-user"></i>
-						</div>
-						Profile
-					</a>
-				</li>
-				<li>
-					<a href="/settings">
-						<div class="icon">
-							<i class="fa-solid fa-gear"></i>
-						</div>
-						Settings
-					</a>
-				</li>
-				<li>
-					<a href="/logout">
-						<div class="icon">
-							<i class="fa-solid fa-arrow-right-from-bracket"></i>
-						</div>
-						Log out
-					</a>
-				</li>
-			</ul>
-		</nav>
-		<form action="">
-			<input type="text" name="username-search" placeholder="Search for People">
-			<button type="submit" class="icon">
-				<i class="fa-solid fa-magnifying-glass"></i>
-			</button>
-		</form>
 	</div>
+
+	<form action="">
+		<input type="text" name="nav-bar-search">
+		<button type="submit">
+			<i class="fa fa-search"></i>
+		</button>
+	</form>
+
+	<div class="nav-menu">
+		<div class="activity">
+			<a href="#">
+				<i class="fa-regular fa-square-plus"></i>
+				<!-- <i class="fa-solid fa-square-plus"></i> -->
+			</a>
+
+			<a href="#">
+				<i class="fa-regular fa-bell"></i>
+				<!-- <i class="fa-solid fa-bell"></i> -->
+			</a>
+		</div>
+
+		<a class="profile" href="#">
+			<div class="pfp">
+				<img src="./src/media/bor3e.JPG" alt="Profile Picture">
+			</div>
+		</a>
+	</div>
+
 </header>
